b473b98e04ba189ad95c6e230c3dea578ac1ad0c
Emerson Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2018-12-11 18:34:45 -0400

    Refactor breakingTheRecords problem solution from HackerRank easy category.

--- hacker_rank/easy/breakingTheRecords.js
+++ hacker_rank/easy/breakingTheRecords.js
@@ -1,61 +1,23 @@
 'use strict';
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
+function countFn(scores, condition) {
+    let count = 0;
+    scores.reduce((curr, val) => {
+        const isMax = condition ? val > curr : val < curr;
+        if (isMax) {
+            curr = val;
+            count++;
+        }
+        return curr;
+    }, scores[0]);
+    return count;
+} 
 
 // Complete the breakingRecords function below.
 function breakingRecords(scores) {
-    let maxV = 0;
-    let minV = 0;
-    scores.reduce((max, val) => {
-        if (val > max) {
-            max = val;
-            maxV++;
-        }
-        return max;
-    }, scores[0]);
+    const maxV = countFn(scores, true);
+    const minV = countFn(scores, false);
 
-    scores.reduce((min, val) => {
-        if (val < min) {
-            min = val;
-            minV++;
-        }
-        return min;
-    }, scores[0]);
     return [maxV, minV];
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const n = parseInt(readLine(), 10);
-
-    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
-
-    const result = breakingRecords(scores);
-
-    ws.write(result.join(' ') + '\n');
-
-    ws.end();
-}
